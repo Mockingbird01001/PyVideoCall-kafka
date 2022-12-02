c1072538790ed010f93c699f12a50fb72cadeea8
BOUFALA Yacine <by33015105@gmail.com>
2022-12-02 14:39:55 +0100

    last one final commit

--- user_2/reciever/consumer.py
+++ user_2/reciever/consumer.py
@@ -2,7 +2,7 @@
 # @Author: BOUFALA Yacine
 # @Date:   2022-11-24 17:54:57
 # @Last Modified by:   BOUFALA Yacine
-# @Last Modified time: 2022-12-02 12:47:47
+# @Last Modified time: 2022-12-02 14:38:02
 
 
 # from audioPlayer import AudioPlayer
@@ -17,7 +17,7 @@ import pyaudio as pyu
 
 class Consumer:
     
-    def __init__(self, host="192.168.111.200:9092", audio_topic='AudioFlux_1', video_topic='ImageFlux_1'):
+    def __init__(self, host="localhost:9092", audio_topic='AudioFlux_1', video_topic='ImageFlux_1'):
         self.KAFKA_ADDR = host
         self.KAFKA_AUDIO_TOPIC = audio_topic
         self.KAFKA_VIDEO_TOPIC = video_topic
