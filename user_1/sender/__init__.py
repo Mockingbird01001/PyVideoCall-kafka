83f193892db783d6cd4161299b73731bb694bbfc
BOUFALA Yacine <by33015105@gmail.com>
2022-12-02 12:48:33 +0100

    very final commit

--- user_1/sender/__init__.py
+++ user_1/sender/__init__.py
@@ -2,7 +2,7 @@
 # @Author: BOUFALA Yacine
 # @Date:   2022-11-25 14:37:00
 # @Last Modified by:   BOUFALA Yacine
-# @Last Modified time: 2022-12-02 10:53:17
+# @Last Modified time: 2022-12-02 12:39:36
 
 from producter import Producer
 from multiprocessing import Process
@@ -19,7 +19,7 @@ def initVideo():
 
 if __name__ == '__main__':
     audio = Process( target =  initAudio)  
-    audio.start()
+    audio.start() 
 
     video = Process( target = initVideo)   
     video.start()
